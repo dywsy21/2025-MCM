4ece2cf9b7d5948cc632440fcd4c0afaec94ce11
antennaterry <terry041020@163.com>
2025-01-26 21:57:49 +0800

    medal country count

--- src/sport_oriented/config.py
+++ src/sport_oriented/config.py
@@ -1,7 +1,7 @@
 # Model configuration
-TARGET_YEAR = 2024
+TARGET_YEAR = 2028
 NUMBER_OF_MATCHES_TO_USE = 8  # Number of previous Olympics to use for training
 GOLD_TOTAL = 329
 SILVER_TOTAL = 330
 BRONZE_TOTAL = 385
-HOST_WEIGHT = 200000000
+HOST_WEIGHT = 1
